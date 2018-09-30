9419a6f800378e24286f6dd79569c3bbc14eecb8
misho <shimonchick@gmail.com>
2018-09-30 17:35:24 +0200

    parsing serial port information and moving a gameobject(not complete)

---
+++ VRGloveDemo/Assets/MovementComponent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class MovementComponent : MonoBehaviour {
+
+
+    SerialPort serialPort;
+    byte[] serialInformation; 
+    void Start () {
+
+        serialPort = new SerialPort("COM3", 9600);
+        serialPort.Open();
+
+        serialInformation = new byte[24]; // 6 floats 4 bytes each
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        string line;
+        line = serialPort.ReadLine();
+  
+        string[] coordinateStrings = line.Split(' ');
+        float[] coordinates = new float[6];
+        for (int i = 0; i < coordinateStrings.Length; i++)
+        {
+            coordinates[i] = float.Parse(coordinateStrings[i]);
+        }
+
+        transform.Rotate(coordinates[0], coordinates[1], coordinates[2]);
+       // transform.Translate(coordinates[3], coordinates[4], coordinates[5]);
+
+    }
+}
