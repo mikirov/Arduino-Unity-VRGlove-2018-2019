bf1e9b94c014a121a49b68a7d8fb8969f404a97e
Krasimir Georgiev <krasi2405@gmail.com>
2018-10-03 13:44:26 +0300

    Stuffs

--- VRGloveDemo/Assets/MovementComponent.cs
+++ VRGloveDemo/Assets/MovementComponent.cs
@@ -12,8 +12,12 @@ public class MovementComponent : MonoBehaviour {
 
     void Start () {
 
-        serialPort = new SerialPort("COM3", 115200);
-        serialPort.Open();
+        serialPort = new SerialPort("COM5", 9600);
+        if (!serialPort.IsOpen)
+        {
+            Debug.LogWarning("Opening port!");
+            serialPort.Open();
+        }
 
         serialInformation = new byte[24]; // 6 floats 4 bytes each
 
@@ -21,16 +25,17 @@ public class MovementComponent : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
         string line;
 
         line = serialPort.ReadLine();
 
- 
-        Debug.Log(line);
+        
+
+        // Debug.Log(line);
 
         string[] coordinateStrings = line.Split(' ');
         float[] coordinates = new float[coordinateStrings.Length];
+        Debug.Log(coordinateStrings.Length);
         for (int i = 0; i < coordinateStrings.Length; i++)
         {
             coordinates[i] = float.Parse(coordinateStrings[i], System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture);
@@ -42,15 +47,18 @@ public class MovementComponent : MonoBehaviour {
             y = coordinates[1];
             z = coordinates[2];
         }
-        transform.rotation = Quaternion.Euler(new Vector3(x, y, z));
-        
-
-
-
+        transform.rotation = Quaternion.Euler(new Vector3(z, x, y));
+     
         //Vector3 position = transform.position + new Vector3(coordinates[3], coordinates[4], coordinates[5]);
         //Quaternion rotation = new Quaternion()
         //transform.Rotate(coordinates[0], coordinates[1], coordinates[2]);
         // transform.Translate(coordinates[3], coordinates[4], coordinates[5]);
 
+    }
+    
+
+    private void OnDisable()
+    {
+        serialPort.Close();
     }
 }
