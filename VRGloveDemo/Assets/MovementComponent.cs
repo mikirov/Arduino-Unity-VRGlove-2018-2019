a623660209932bcf99297b4541239ac20477ebe7
Krasimir Georgiev <krasi2405@gmail.com>
2018-10-07 23:08:24 +0300

    Very simple glove for testering purposes only.

--- VRGloveDemo/Assets/MovementComponent.cs
+++ VRGloveDemo/Assets/MovementComponent.cs
@@ -10,13 +10,21 @@ public class MovementComponent : MonoBehaviour {
 
     [SerializeField]
     string port = "COM3";
+
+    private GloveController gloveController;
    
     float x = 0, y = 0, z = 0;
     string line; // a single line read from serial port
     string[] coordinateStrings = new string[3];
 
 
+
     void Start () {
+        gloveController = FindObjectOfType<GloveController>();
+        if(!gloveController)
+        {
+            Debug.LogError("No glove controller in scene!");
+        }
 
         serialPort = new SerialPort(port, 38400);
         if (!serialPort.IsOpen)
@@ -30,7 +38,6 @@ public class MovementComponent : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
         line = serialPort.ReadLine();
      
         coordinateStrings = line.Split(' ');
@@ -41,12 +48,15 @@ public class MovementComponent : MonoBehaviour {
         z = coordinates[2];
         
         transform.rotation = Quaternion.Euler(new Vector3(z, x, y));
-     
+
         //Vector3 position = transform.position + new Vector3(coordinates[3], coordinates[4], coordinates[5]);
         //Quaternion rotation = new Quaternion()
         //transform.Rotate(coordinates[0], coordinates[1], coordinates[2]);
         // transform.Translate(coordinates[3], coordinates[4], coordinates[5]);
-
+        Vector3 wristAccData = Vector3.zero;
+        Vector3 gyroData = new Vector3(z, x, y);
+        Vector3[] fingersAccData = { Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero };
+        gloveController.FeedMovementInfo(wristAccData, gyroData, fingersAccData);
     }
     
 
