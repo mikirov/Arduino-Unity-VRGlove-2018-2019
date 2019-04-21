af09370afd95a5eb57495f3b5407f39b8bfbca2e
shimonchick <shimonchick@gmail.com>
2019-04-21 20:12:33 +0300

    Nodemcu code and multiplexer library

---
+++ Arduino/Multiplexer/Multiplexer.h
@@ -0,0 +1,29 @@
+//
+// Created by misho on 20/04/19.
+//
+
+#ifndef NODEMCU_MULTIPLEXER_H
+#define NODEMCU_MULTIPLEXER_H
+
+
+#define S0 0
+#define S1 4
+#define S2 5
+#define S3 16
+#define SIG 17
+
+
+class Multiplexer {
+public:
+    explicit Multiplexer(int input_count_param) : input_count(input_count_param){}
+    void setup();
+    int readInput(unsigned int number);
+    void readAllInputs(int results[]);
+private:
+    int input_count;
+    int select_pins[4] = {S0, S1, S2, S3};
+    int signal_pin;
+};
+
+
+#endif //NODEMCU_MULTIPLEXER_H
---
+++ Arduino/VRGlove.ino
@@ -0,0 +1,68 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include "Multiplexer.h"
+
+#define INPUT_COUNT 11
+
+Multiplexer multiplexer(INPUT_COUNT);
+int results[INPUT_COUNT];
+
+const char* ssid = "velios1";//put your wifi network name here
+const char* password = "789123654";//put your wifi password here
+
+IPAddress ip(192,168,1,6);
+IPAddress gateway(192,168,1,1);
+IPAddress subnet(255,255,255,0);
+
+WiFiServer server(80);
+WiFiClient client;
+
+
+void setup() {
+  Serial.begin(9600);
+  WiFi.begin(ssid, password);
+  WiFi.config(ip, gateway, subnet);
+  Serial.println("Connecting");
+
+  while(WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.println(".");
+  }
+
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  //Start the TCP server
+  server.begin();
+  Serial.printf("Web server started, open %s in a web browser\n", WiFi.localIP().toString().c_str());
+}
+
+void loop() {
+  client = server.available();
+
+  if(client)
+  {
+    Serial.println("Client Connected");
+    while(client.connected())
+    {
+      multiplexer.readAllInputs(results);
+      for(int i = 0; i < INPUT_COUNT; i++){
+        Serial.printf("input: %d value: %d\n", i + 1, results[i]);
+        client.print(results[i]);
+        client.print(' ');
+      
+      }
+
+        client.print('\r');
+      
+    
+      //Delay before the next reading
+      delay(10);
+    }
+  }
+
+}
